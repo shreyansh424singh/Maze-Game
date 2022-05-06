3bc71b44ffe94777e871923b8dc52f59f52a8690
shreyansh424singh <77938272+shreyansh424singh@users.noreply.github.com>
2022-05-06 14:54:56 +0530

    Add files via upload

--- game/include/Dot.hpp
+++ game/include/Dot.hpp
@@ -3,7 +3,9 @@
 #include<bits/stdc++.h>
 
 #include "LTexture.hpp"
-#include "back1.h"
+#include "server.h"
+#include "client.h"
+#include "Maparray.h"
 
 using namespace std;
 
@@ -18,6 +20,11 @@ class Dot
 		//Maximum axis velocity of the dot
 		int DOT_VEL = 1;
 
+		// Score, yulu
+		int mScore, mYulu;
+
+		int mDestX, mDestY, mDestReached;
+
         vector<int> temp;
         int backarr[1920][1080];
 
@@ -29,7 +36,11 @@ class Dot
 		void handleEventN( SDL_Event& e );
 
 		//Moves the dot and checks collision
-		void move(int SCREEN_HEIGHT, int SCREEN_WIDTH);
+		void move(int SCREEN_HEIGHT, int SCREEN_WIDTH, int usr_id, int sys_sock);
+		void move_P2(int usr_id, int sys_sock);
+
+		void sendPos(int usr_id, int msock);
+		void getPos(int usr_id, int msock);
 
 		//Shows the dot on the screen
 		void render(LTexture* gDotTexture, SDL_Renderer*& gRenderer);
@@ -37,6 +48,8 @@ class Dot
         bool checkCollision( SDL_Rect a);
 
 		void setInitialPosition(char p);
+
+		bool checkDestReached();
 
     private:
 		//The X and Y offsets of the dot
